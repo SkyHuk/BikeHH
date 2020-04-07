6a919c3ad38b415f16a17f2269fc455a137847e2
Julian Gersch <jg@wps.de>
2020-04-07 18:46:32 +0200

    Anpassen der Standardview der Karte auf Hamburg. Vereinfachung des OpenStreetMap Tilelayers

--- Webapp/src/app/components/map/map.component.ts
+++ Webapp/src/app/components/map/map.component.ts
@@ -1,10 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { defaults as defaultControls } from 'ol/control';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-import ZoomToExtent from 'ol/control/ZoomToExtent';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-map',
@@ -23,23 +21,13 @@ export class MapComponent implements AfterViewInit {
       target: 'map',
       layers: [
         new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
+          source: new OSM()
         })
       ],
       view: new View({
-        center: [813079.7791264898, 5929220.284081122],
-        zoom: 7
-      }),
-      controls: defaultControls().extend([
-        new ZoomToExtent({
-          extent: [
-            813079.7791264898, 5929220.284081122,
-            848966.9639063801, 5936863.986909639
-          ]
-        })
-      ])
+        center: [1115000.0, 7095000.0],
+        zoom: 12
+      })
     });
   }
 
