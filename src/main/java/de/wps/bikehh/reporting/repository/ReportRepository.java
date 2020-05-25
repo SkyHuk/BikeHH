2e2eec4667f2d63206e7b48d37aad99fbb573d9e
Julian Gersch <jg@wps.de>
2020-05-25 15:19:57 +0200

    Entfernen des Beispiel-Reporting packages

--- src/main/java/de/wps/bikehh/reporting/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package de.wps.bikehh.reporting.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import de.wps.bikehh.reporting.material.Report;
-
-public interface ReportRepository extends CrudRepository<Report, Long> {
-
-}
