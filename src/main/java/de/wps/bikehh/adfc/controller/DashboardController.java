54d2c09e2295723d61d552438ff01a27540bb9f2
FelixWolf <wolf@sonobeacon.com>
2020-05-29 16:00:49 +0200

    dashboard added, does not work though

---
+++ src/main/java/de/wps/bikehh/adfc/controller/DashboardController.java
@@ -0,0 +1,15 @@
+package de.wps.bikehh.adfc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("Übersicht")
+public class DashboardController {
+
+	@GetMapping
+	public String showDashboard() {
+		return "adfc/dashboard";
+	}
+}
