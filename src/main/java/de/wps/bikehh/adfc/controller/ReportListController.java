400bd866635f6f4d5a470aaacf6152fc414e85b3
Felix Wolf <x3mdroid@gmail.com>
2020-05-29 13:52:46 +0200

    @GetMapping missing

--- src/main/java/de/wps/bikehh/adfc/controller/ReportListController.java
+++ src/main/java/de/wps/bikehh/adfc/controller/ReportListController.java
@@ -1,17 +1,14 @@
 package de.wps.bikehh.adfc.controller;
 
 import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-// import de.wps.bikehh.benutzerverwaltung.material.User;
-// import de.wps.bikehh.willkommen.material.Bier;
 
 @Controller
 @RequestMapping("reportList")
 public class ReportListController {
+	
+	@GetMapping
 	public String showReportList() {
 		return "adfc/reportList";
 	}
