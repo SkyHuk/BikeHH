81b722fd79d0a41fc26e5b37bb9f0da0d859b065
Matz Radloff <matzradloff@gmail.com>
2020-06-08 18:30:06 +0200

    Updated single survey URL. Started create-survey rewrite using vuejs.

--- src/main/java/de/wps/bikehh/adfc/controller/SingleSurveyController.java
+++ src/main/java/de/wps/bikehh/adfc/controller/SingleSurveyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +13,7 @@ import de.wps.bikehh.adfc.material.SurveyTest;
 import de.wps.bikehh.utilities.Utils;
 
 @Controller
-@RequestMapping("umfrage")
+@RequestMapping("umfragen/{surveyId}")
 public class SingleSurveyController {
 
 	/**
@@ -20,15 +21,15 @@ public class SingleSurveyController {
 	 * opens html page for specific survey
 	 * 
 	 * @param model        spring model
-	 * @param surveyNumber number of survey to open
+	 * @param surveyId survey id to open
 	 * @return html page
 	 */
 	@GetMapping
-	public String showSingleSurvey(Model model, @RequestParam(name = "values") int surveyNumber) {
+	public String showSingleSurvey(Model model, @PathVariable Integer surveyId) {
 
 		List<SurveyTest> surveysList = Utils.getSurveyJsonsAsArray();
 		for (SurveyTest survey : surveysList) {
-			if (survey.getId() == surveyNumber) {
+			if (survey.getId() == surveyId) {
 				model.addAttribute("survey", survey);
 			}
 		}
