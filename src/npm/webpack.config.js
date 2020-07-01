8f1a160a94b5cba35240ade17f673c843da1c0a9
Matz Radloff <matzradloff@gmail.com>
2020-07-01 23:19:10 +0200

    Started separating js code. WIP webpack.

--- src/npm/webpack.config.js
+++ src/npm/webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = 
+module.exports =
 [{
   entry: './src/bundle/index.js',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, '../main/resources/static/generated/js'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['file-loader', 'css-loader'],
+      },
+      {
+        test: /\.png$/i,
+        use: ['file-loader'],
+      },
+    ]
+  },
 }];
