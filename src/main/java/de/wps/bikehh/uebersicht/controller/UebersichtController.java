40ec0d77dd821f628184f163450651ba4f2abf07
Julian Gersch <gerschmaster@googlemail.com>
2020-07-31 14:32:02 +0200

    Refactor Aufräumen von Kommentaren im UebersichtController

--- src/main/java/de/wps/bikehh/uebersicht/controller/UebersichtController.java
+++ src/main/java/de/wps/bikehh/uebersicht/controller/UebersichtController.java
@@ -8,31 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Controller für das Home-Verzeichnis "/".
  * 
  * @author felixwolf
- *
  */
 @Controller
 @RequestMapping("/")
 public class UebersichtController {
 
-	// TODO: Diese Klasse muss getestet werden, als Beispiel siehe andere Controller
-
-	/**
-	 * redirected von "/" nach "/uebersicht"
-	 * 
-	 * @return der redirect
-	 */
 	@GetMapping
 	public String home() {
 		return "redirect:/uebersicht";
 	}
 
 	/**
-	 * gibt das HTML-Template für die Übersicht zurück
+	 * Gibt das HTML-Template für die Übersicht zurück
 	 * 
-	 * TODO: template ist leer, ausbauen. Mögliche Ideen: Statistiken über Nutzer,
-	 * Umfragen und Meldungen
+	 * TODO: template ist leer, ausbauen. Mögliche Ideen: Statistiken über
+	 * Nutzer, Umfragen und Meldungen
 	 * 
-	 * @return das template
+	 * @return Das Template
 	 */
 	@GetMapping("uebersicht")
 	public String zeigeUebersicht() {
