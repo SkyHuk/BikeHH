150baddf873ba7cd8ea50e442163f26561a395e0
Julian Gersch <gerschmaster@googlemail.com>
2020-08-05 12:11:41 +0200

    Refactor mit dem Besen durchfegen durch den Kartenbereich

--- src/main/java/de/wps/bikehh/karte/controller/KartenController.java
+++ src/main/java/de/wps/bikehh/karte/controller/KartenController.java
@@ -8,44 +8,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import de.wps.bikehh.umfragen.material.Umfrage;
-import de.wps.bikehh.umfragen.service.UmfragenService;
+import de.wps.bikehh.umfragen.applicationservice.UmfragenApplicationService;
+import de.wps.bikehh.umfragen.dto.UmfrageDto;
 
-/**
- * Dieser Controller ist der Anlaufpunkt für alle Requests auf "/karte"
- * 
- *
- */
 @Controller
-@RequestMapping("karte")
+@RequestMapping("map")
 public class KartenController {
 
-	private UmfragenService umfragenService;
+	private UmfragenApplicationService umfragenAppService;
 
-	/**
-	 * Benötigt für JUNIT tests
-	 * 
-	 * @param umfragenService der Service
-	 */
 	@Autowired
-	public KartenController(UmfragenService umfragenService) {
-		this.umfragenService = umfragenService;
+	public KartenController(UmfragenApplicationService umfragenAppService) {
+		this.umfragenAppService = umfragenAppService;
 	}
 
-	/**
-	 * gibt das HTML-Template für "/karte" zurück in das model werden alle Umfragen
-	 * geladen, um sie auf der Karte anzuzeigen (mit den kleinem Markern, die
-	 * Informationen zu der Umfrage beihalten
-	 * 
-	 * @param model springboot model mit allesn Umfragen
-	 * @return das HTML-Template für Karte
-	 */
 	@GetMapping
-	public String zeigeKarte(Model model) {
-
-		List<Umfrage> umfragen = umfragenService.getAlleUmfragen();
-
+	public String getMap(Model model) {
+		List<UmfrageDto> umfragen = umfragenAppService.getAlleUmfragen();
 		model.addAttribute("umfragen", umfragen);
-		return "karte/karte";
+		return "map/map";
 	}
 }
--- src/test/java/de/wps/bikehh/karte/KartenControllerTest.java
+++ src/test/java/de/wps/bikehh/karte/KartenControllerTest.java
@@ -1,57 +1,53 @@
 package de.wps.bikehh.karte;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.Model;
 
-import com.google.gson.Gson;
-
 import de.wps.bikehh.karte.controller.KartenController;
-import de.wps.bikehh.umfragen.material.Umfrage;
-import de.wps.bikehh.umfragen.repository.UmfragenRepository;
-import de.wps.bikehh.umfragen.service.UmfragenService;
+import de.wps.bikehh.umfragen.applicationservice.UmfragenApplicationService;
+import de.wps.bikehh.umfragen.dto.UmfrageDto;
 
 public class KartenControllerTest {
 
-	private UmfragenRepository umfragenRepository;
-	private UmfragenService umfragenService;
+	private UmfragenApplicationService umfragenAppService;
 	private KartenController kartenController;
 	private Model model;
 
-	public KartenControllerTest() {
-		// mocke Repository
-		this.umfragenRepository = mock(UmfragenRepository.class);
+	@Before
+	public void init() {
+		model = new ConcurrentModel();
+		umfragenAppService = mock(UmfragenApplicationService.class);
+		kartenController = new KartenController(umfragenAppService);
 
-		// mocke Service
-		this.umfragenService = new UmfragenService(this.umfragenRepository);
+		UmfrageDto testUmfrage = new UmfrageDto();
+		testUmfrage.setId(1);
 
-		// KartenController
-		this.kartenController = new KartenController(umfragenService);
+		List<UmfrageDto> alleUmfragen = new ArrayList<>();
+		alleUmfragen.add(testUmfrage);
 
-		// mock Model
-		model = mock(Model.class);
+		when(umfragenAppService.getAlleUmfragen()).thenReturn(alleUmfragen);
 	}
 
 	@Test
-	public void zeigeKarteTest() {
-		String jsonString = "{\"bestaetigtVonUsern\":[],\"ersteller\":{\"id\":1,\"emailAddress\":\"admin@chef.lol\",\"encryptedPassword\":\"sha: uwXzywW1mNUAWQZIaRA3cvRyyQQ=\",\"isVerified\":false,\"isLocked\":false,\"credibility\":0,\"privacySetting\":0,\"role\":\"administrator\",\"bestaetigteUmfragen\":[],\"updatedAt\":\"Jul 8, 2020, 8:28:03 PM\",\"createdAt\":\"Jul 8, 2020, 8:28:03 PM\"},\"erstelltAmDatum\":\"2020-07-08T18:30:38.652Z\",\"umfrageDisabled\":false,\"fahrtrichtung\":-2.1142974543149453,\"bearbeitet\":false,\"manuellErstellt\":true,\"titel\":\"Test\",\"startDatum\":\"2020-07-08\",\"endDatum\":\"2020-07-31\",\"bestaetigtSchwellenwert\":10,\"fragen\":[{\"titel\":\"Frage1\",\"antwortMoeglichkeiten\":[{\"text\":\"Ja\"},{\"text\":\"Nein\"},{\"text\":\"Vielleicht\"}],\"bedingungen\":[],\"fahrtrichtung\":0,\"erlaubeBenutzerdefinierteAntwort\":false},{\"titel\":\"Frage2\",\"antwortMoeglichkeiten\":[{\"text\":\"Ja\"}],\"bedingungen\":[{\"frage\":\"Frage1\",\"erwarteteAntwort\":\"Vielleicht\"}],\"fahrtrichtung\":0,\"erlaubeBenutzerdefinierteAntwort\":true}],\"kategorie\":{\"name\":\"Verkehrsführung\"},\"laengengrad\":53.56261550524817,\"breitengrad\":10.00133514404297,\"adresse\":{\"stadt\":\"Hamburg\",\"postleitZahl\":\"20148\",\"strasse\":\"Alsterufer\"}}";
-		Umfrage umfrage = new Gson().fromJson(jsonString, Umfrage.class);
-		List<Umfrage> umfragen = new ArrayList<Umfrage>();
-		umfragen.add(umfrage);
+	public void testgetMap_enthaeltUmfragenUndGibtTemplate() {
+		// act
+		String redirectStr = kartenController.getMap(model);
 
-		when(umfragenRepository.findAll()).thenReturn(umfragen);
-
-		kartenController.zeigeKarte(model);
-		Mockito.verify(umfragenRepository, times(1)).findAll();
-
-		assertEquals("karte/karte", this.kartenController.zeigeKarte(model));
+		// assert
+		verify(umfragenAppService, times(1)).getAlleUmfragen();
+		assertTrue(model.containsAttribute("umfragen"));
+		assertEquals("map/map", redirectStr);
 	}
 }
