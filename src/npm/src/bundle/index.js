322494356bf12016716a93068749d9c16998590a
Julian Gersch <gerschmaster@googlemail.com>
2020-08-14 17:29:43 +0200

    Refactor Entfernen von ungenutzten Templates und Javascript-Bibliotheken.

--- src/npm/src/bundle/index.js
+++ src/npm/src/bundle/index.js
@@ -1,11 +1,5 @@
 import 'jquery';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free';
-import 'leaflet';
-import 'leaflet-polylinedecorator';
-import 'leaflet/dist/leaflet.css';
 
 import './map';
-import './umfrage-erstellen';
-import './umfrage';
-import './karte';
--- src/npm/webpack.config.js
+++ src/npm/webpack.config.js
@@ -8,17 +8,5 @@ module.exports =
   output: {
     path: path.resolve(__dirname, '../main/resources/static/generated/js'),
     filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['file-loader', 'css-loader'],
-      },
-      {
-        test: /\.png$/i,
-        use: ['file-loader'],
-      },
-    ]
-  },
+  }
 }];
