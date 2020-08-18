646afff571281bca53a4625ea2d9a2359397cf6b
Julian Gersch <gerschmaster@googlemail.com>
2020-08-18 10:36:55 +0200

    Fix gitignore to include db package

---
+++ src/main/java/de/wps/bikehh/framework/db/converter/StringListConverter.java
@@ -0,0 +1,22 @@
+package de.wps.bikehh.framework.db.converter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+	private static final String SPLIT_CHAR = ";";
+
+	@Override
+	public String convertToDatabaseColumn(List<String> stringList) {
+		return String.join(SPLIT_CHAR, stringList);
+	}
+
+	@Override
+	public List<String> convertToEntityAttribute(String string) {
+		return Arrays.asList(string.split(SPLIT_CHAR));
+	}
+}
