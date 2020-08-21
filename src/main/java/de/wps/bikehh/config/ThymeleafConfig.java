99c03b98940bafed8fdd77430852d04cc32b9776
Julian Gersch <gerschmaster@googlemail.com>
2020-08-21 10:27:53 +0200

    Refactor Aufräumen der ThymeleafConfig

--- src/main/java/de/wps/bikehh/config/ThymeleafConfig.java
+++ src/main/java/de/wps/bikehh/config/ThymeleafConfig.java
@@ -1,43 +1,16 @@
 package de.wps.bikehh.config;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.thymeleaf.templateresolver.StringTemplateResolver;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @Configuration
 public class ThymeleafConfig {
+
 	@Bean
 	public LayoutDialect layoutDialect() {
 		return new LayoutDialect();
 	}
 
-	/*@Bean
-	public SpringTemplateEngine springTemplateEngine() {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.addTemplateResolver(htmlTemplateResolver());
-		//templateEngine.addDialect(new LayoutDialect());
-		return templateEngine;
-	}
-
-	@Bean
-	public SpringResourceTemplateResolver htmlTemplateResolver(){
-		SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
-		emailTemplateResolver.setOrder(1);
-		emailTemplateResolver.setPrefix("classpath:/templates/");
-		emailTemplateResolver.setSuffix(".html");
-		emailTemplateResolver.setTemplateMode("HTML");
-		emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-		return emailTemplateResolver;
-	}*/
 }
