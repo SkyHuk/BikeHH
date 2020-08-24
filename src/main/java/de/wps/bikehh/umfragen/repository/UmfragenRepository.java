4a03d3cc15d3cfef3883ca4424004bc87c235af6
Julian Gersch <gerschmaster@googlemail.com>
2020-08-24 14:23:11 +0200

    Refactor aufhübschen des UmfragenRepositories.

--- src/main/java/de/wps/bikehh/umfragen/repository/UmfragenRepository.java
+++ src/main/java/de/wps/bikehh/umfragen/repository/UmfragenRepository.java
@@ -1,11 +1,11 @@
 package de.wps.bikehh.umfragen.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import de.wps.bikehh.umfragen.material.Umfrage;
 
-/**
- * Repository-Interface für Datenbank-Einbindung.
- */
+@Repository
 public interface UmfragenRepository extends CrudRepository<Umfrage, Long> {
+
 }
